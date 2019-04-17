3dcd4aec9ccf0785aa2379848da0118a01dbb72d
vaishnavigandhi04 <vaishnavigandhi04@gmail.com>
2019-04-16 19:16:35 -0700

    third commit

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
-var app = express();
+const port = port = process.env.PORT || 8080;
 
-app.get('/', (request, response) => {
-    response.send('<h1> Hello Express!</h1>');
+// var app = express();
+//
+// app.get('/', (request, response) => {
+//     response.send('<h1> Hello Express!</h1>');
+// });
+
+app.listen(port,() => {
+    console.log(`Server is up on the port ${port}`);
 });
-
-app.listen(8080);
